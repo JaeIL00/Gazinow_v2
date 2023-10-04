c53d0b04e2f0e9ec411919c39562c9898781fe21
ShinJaeIl01 <ekvm555@naver.com>
2023-10-04 14:42:47 +0900

    [Test]로그인 버튼 터치 테스트 작성 #5

--- __tests__/unit/login.test.tsx
+++ __tests__/unit/login.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
-import { describe, expect, it } from '@jest/globals';
+import { fireEvent, render, waitFor } from '@testing-library/react-native';
+import { describe, expect, it, jest } from '@jest/globals';
 import { Login } from '@/components/auth/page';
 
 describe('<Login />', () => {
@@ -25,5 +25,20 @@ describe('<Login />', () => {
     getByDisplayValue(email);
     fireEvent(inputPassword, 'changeText', password);
     getByDisplayValue(password);
+  });
+  it('touch login button', async () => {
+    const email = 'test@naver.com';
+    const password = '1234qwer!';
+    const { getByPlaceholderText, getByText } = render(<Login />);
+    const inputEmail = getByPlaceholderText('이메일을 입력해주세요');
+    const inputPassword = getByPlaceholderText('비밀번호를 입력해주세요');
+    fireEvent(inputEmail, 'changeText', email);
+    fireEvent(inputPassword, 'changeText', password);
+    const button = getByText('로그인');
+    fireEvent(button, 'press');
+    getByText('로딩중');
+    await waitFor(() => {
+      expect(getByText('로딩중')).not.toBeDefined();
+    });
   });
 });
