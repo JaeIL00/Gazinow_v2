8dcb804ec84562fe37021cfa9420ef2567a2c195
ShinJaeIl01 <ekvm555@naver.com>
2023-10-04 11:46:15 +0900

    [Test] 로그인 Input 테스트 작성 #5

--- __tests__/unit/login.test.tsx
+++ __tests__/unit/login.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import { describe, expect, it } from '@jest/globals';
 import { Login } from '@/components/auth/page';
 
@@ -14,5 +14,16 @@ describe('<Login />', () => {
     getByPlaceholderText('이메일을 입력해주세요');
     getByPlaceholderText('비밀번호를 입력해주세요');
     getByText('로그인');
+  });
+  it('changes input', () => {
+    const email = 'test@naver.com';
+    const password = '1234qwer!';
+    const { getByPlaceholderText, getByDisplayValue } = render(<Login />);
+    const inputEmail = getByPlaceholderText('이메일을 입력해주세요');
+    const inputPassword = getByPlaceholderText('비밀번호를 입력해주세요');
+    fireEvent(inputEmail, 'changeText', email);
+    getByDisplayValue(email);
+    fireEvent(inputPassword, 'changeText', password);
+    getByDisplayValue(password);
   });
 });
