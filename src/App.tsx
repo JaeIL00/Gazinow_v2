731c3a903217ec9910b5fdec9287d3389fa47826
ShinJaeIl01 <ekvm555@naver.com>
2023-10-10 21:54:55 +0900

    [Chore] redux-toolkt 설치 및 환경 구성 #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
 import styled from 'styled-components/native';
 
 import { RootNavigation } from '@/navigation';
+import { store } from '@/store/store';
 
 const queryClient = new QueryClient();
 
 const App = (): JSX.Element => {
   return (
     <SafeArea>
-      <QueryClientProvider client={queryClient}>
-        <RootNavigation />
-      </QueryClientProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <RootNavigation />
+        </QueryClientProvider>
+      </Provider>
     </SafeArea>
   );
 };
