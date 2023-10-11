26d5b728ba156d6cbcec3c028fd0d8b4b99ea08c
ShinJaeIl01 <ekvm555@naver.com>
2023-10-11 12:52:18 +0900

    [Feat] 로그인 스크린 로그인 유무 조건 추가 #5

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -5,7 +5,8 @@ import type { StackNavigationProp } from '@react-navigation/stack';
 import { Login } from '@/components/auth/page';
 import { LOGIN, MAIN_BOTTOM_TAB, SEARCH_NAVIGATION } from '@/constants/navigation';
 import { MainBottomTabNavigation, SearchNavigation } from '@/navigation';
-import { RootStackParamList } from '@/types/navigation';
+import { useAppSelect } from '@/store';
+import type { RootStackParamList } from '@/types/navigation';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
@@ -14,10 +15,12 @@ const screenOption = {
 };
 
 const RootNavigation = () => {
+  const accessToken = useAppSelect(({ auth }) => auth.accessToken);
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={screenOption}>
-        <Stack.Screen name={LOGIN} component={Login} />
+        {!accessToken && <Stack.Screen name={LOGIN} component={Login} />}
         <Stack.Screen name={MAIN_BOTTOM_TAB} component={MainBottomTabNavigation} />
         <Stack.Screen name={SEARCH_NAVIGATION} component={SearchNavigation} />
       </Stack.Navigator>
