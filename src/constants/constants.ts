e3c8c2e9afc7929ed31b8b50d08fe8ecbd6d0f3d
ShinJaeIl01 <ekvm555@naver.com>
2023-10-13 12:00:16 +0900

    [Feat] 지하철역 정보 공공데이터 스토리지 저장 #6

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,2 @@
-export const AUTH_STORAGE_KEY = 'user_access_token' as const;
+export const AUTH_STORAGE_KEY = 'user_token' as const;
+export const SUBWAY_INFO_STORAGE_KEY = 'subway_info' as const;
--- src/hooks/queries/publicDataQuery.ts
+++ src/hooks/queries/publicDataQuery.ts
@@ -1,10 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useQuery } from 'react-query';
 
 import { subwayInfoFetch } from '@/apis/publicData';
+import { SUBWAY_INFO_STORAGE_KEY } from '@/constants';
+import { SubwayInfoResponse } from '@/types/apis';
+
+const setInfoStorage = async (data: SubwayInfoResponse) => {
+  try {
+    const response = JSON.stringify(data);
+    await AsyncStorage.setItem(SUBWAY_INFO_STORAGE_KEY, response);
+  } catch (error) {
+    // debug
+    console.error('Failed subway info set storage');
+  }
+};
 
 export const useSubwayInfoQuery = () => {
   const { isSuccess } = useQuery('subwayInfo', subwayInfoFetch, {
-    onSuccess({ data }) {},
+    onSuccess({ data }) {
+      setInfoStorage(data);
+    },
     onError(error) {
       // debug
       console.error('Get failed subway info from Seoul public data ', error);
