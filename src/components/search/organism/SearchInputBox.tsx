ad3f9871a14f72a5b66aa298b5dfd9d7c4f53241
ShinJaeIl01 <ekvm555@naver.com>
2023-10-22 18:34:53 +0900

    [Feat] 지하철 검색 인풋창 버튼 기능 및 인풋 기능 구현 #6

--- src/components/search/organism/SearchInputBox.tsx
+++ src/components/search/organism/SearchInputBox.tsx
@@ -1,21 +1,52 @@
 import styled from '@emotion/native';
+import { useCallback, useState } from 'react';
 
 import { Input } from '@/components/common/atoms';
 import { IconButton } from '@/components/common/molecules';
+import { useRootNavigation } from '@/navigation/RootNavigation';
 import { useAppSelect } from '@/store';
 
 const SearchInputBox = () => {
+  const rootNavigation = useRootNavigation();
   const stationType = useAppSelect(({ subwaySearch }) => subwaySearch.stationType);
+
+  const [searchText, setSearchText] = useState<string>('');
+
+  const backToScreen = useCallback(() => {
+    rootNavigation.goBack();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const changeSearchText = useCallback((text: string) => {
+    setSearchText(text);
+  }, []);
+
+  const deleteInputText = useCallback(() => {
+    setSearchText('');
+  }, []);
 
   return (
     <Container>
-      <IconButton isFontIcon iconName="arrow-back-sharp" iconWidth="19.5" iconColor="#49454F" />
-      <SearchInput placeholder={`${stationType}역을 검색해보세요`} placeholderTextColor="#BEBEBE" />
+      <IconButton
+        isFontIcon
+        iconName="arrow-back-sharp"
+        iconWidth="19.5"
+        iconColor="#49454F"
+        onPress={backToScreen}
+      />
+      <SearchInput
+        value={searchText}
+        placeholder={`${stationType}역을 검색해보세요`}
+        placeholderTextColor="#BEBEBE"
+        inputMode="search"
+        onChangeText={changeSearchText}
+      />
       <IconButton
         isFontIcon
         iconName="close-circle"
         iconWidth="19.5"
         iconColor="rgba(0, 0, 0, 0.46)"
+        onPress={deleteInputText}
       />
     </Container>
   );
