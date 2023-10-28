b9a3f937fe4115e56afb9dcbb6c7700750af7d44
ShinJaeIl01 <ekvm555@naver.com>
2023-10-28 22:27:48 +0900

    [Feat] 지하철역 공공데이터 호선 값 수정 로직 추가 #6

--- src/hooks/queries/publicDataQuery.ts
+++ src/hooks/queries/publicDataQuery.ts
@@ -9,10 +9,7 @@ import type { SubwayPublicDataTypes } from '@/types/apis';
 
 const setInfoStorage = async (data: SubwayPublicDataTypes[]) => {
   try {
-    const sortedArr = data.sort((a, b) =>
-      a.STATION_NM < b.STATION_NM ? -1 : a.STATION_NM > b.STATION_NM ? 1 : 0,
-    );
-    const response = JSON.stringify(sortedArr);
+    const response = JSON.stringify(data);
     await AsyncStorage.setItem(SUBWAY_INFO_STORAGE_KEY, response);
   } catch (error) {
     // debug
@@ -20,13 +17,28 @@ const setInfoStorage = async (data: SubwayPublicDataTypes[]) => {
   }
 };
 
+const remakeData = (data: SubwayPublicDataTypes[]) => {
+  const sortedArr = data.sort((a, b) =>
+    a.STATION_NM < b.STATION_NM ? -1 : a.STATION_NM > b.STATION_NM ? 1 : 0,
+  );
+  const lineNumFresh = sortedArr.map((info) => {
+    const lineNumFirstZero = info.LINE_NUM.split('')[0] === '0';
+    return {
+      ...info,
+      LINE_NUM: lineNumFirstZero ? info.LINE_NUM.replace('0', '') : info.LINE_NUM,
+    };
+  });
+  return lineNumFresh;
+};
+
 export const useSubwayInfoQuery = () => {
   const dispatch = useAppDispatch();
   const { isSuccess, refetch: subwayInfoFetching } = useQuery('subwayInfo', subwayInfoFetch, {
     enabled: false,
     onSuccess({ data }) {
-      setInfoStorage(data.SearchSTNBySubwayLineInfo.row);
-      dispatch(getSubwayPublicData(data.SearchSTNBySubwayLineInfo.row));
+      const freshData = remakeData(data.SearchSTNBySubwayLineInfo.row);
+      setInfoStorage(freshData);
+      dispatch(getSubwayPublicData(freshData));
     },
     onError(error) {
       // debug
