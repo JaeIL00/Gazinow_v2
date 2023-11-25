25c9408bd85a10ae1461bd2c4b38f0799187bbbd
ShinJaeIl01 <ekvm555@naver.com>
2023-11-25 19:58:17 +0900

    아이콘 추가

--- src/assets/icons/iconPath.ts
+++ src/assets/icons/iconPath.ts
@@ -3,9 +3,13 @@ import { ImageSourcePropType } from 'react-native';
 export interface IconPathTypes {
   exchange_gray: ImageSourcePropType;
   location_pin_gray: ImageSourcePropType;
+  left_arrow_nonbar: ImageSourcePropType;
+  right_arrow_nonbar: ImageSourcePropType;
 }
 
 export const iconPath: IconPathTypes = {
   exchange_gray: require('@/assets/icons/exchange_gray.png'),
   location_pin_gray: require('@/assets/icons/location_pin_gray.png'),
+  left_arrow_nonbar: require('@/assets/icons/left_arrow_nonbar.png'),
+  right_arrow_nonbar: require('@/assets/icons/right_arrow_nonbar.png'),
 };
