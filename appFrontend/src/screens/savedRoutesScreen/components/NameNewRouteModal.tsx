3ea061f975acfb8d20408efae44ba5455a876d60
seoyeon <ryd10227@gmail.com>
2024-02-10 22:41:02 +0900

    새 경로 저장 시 이름 넣는 화면의 버튼을 풀블리드버튼으로 수정, KeyboardAwareScrollView 라이브러리 설치

--- appFrontend/src/screens/savedRoutesScreen/components/NameNewRouteModal.tsx
+++ appFrontend/src/screens/savedRoutesScreen/components/NameNewRouteModal.tsx
@@ -6,8 +6,9 @@ import { useSaveMyRoutesQuery } from '@/global/apis/hook';
 import { useQueryClient } from 'react-query';
 import { SubwaySimplePath } from '@/global/components';
 import { Path, SubPath } from '@/global/apis/entity';
-import { Image, View } from 'react-native';
+import { Image, View, Keyboard, Platform } from 'react-native';
 import { iconPath } from '@/assets/icons/iconPath';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 interface ModalProps {
   item: Path;
@@ -18,7 +19,14 @@ interface ModalProps {
 const NameNewRouteModal = ({ item, onCancel, setDepth }: ModalProps) => {
   const [roadName, setRoadName] = useState<string>();
   const [isDuplicatedName, setIsDuplicatedName] = useState<boolean>(false);
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false);
   const queryClient = useQueryClient();
+
+  const keyboardDidShow = () => setKeyboardVisible(true);
+  const keyboardDidHide = () => setKeyboardVisible(false);
+
+  Keyboard.addListener('keyboardDidShow', keyboardDidShow);
+  Keyboard.addListener('keyboardDidHide', keyboardDidHide);
 
   const freshSubPathData: SubPath[] = useMemo(() => {
     const subPaths = item?.subPaths || [];
@@ -30,8 +38,6 @@ const NameNewRouteModal = ({ item, onCancel, setDepth }: ModalProps) => {
       await queryClient.invalidateQueries('getRoads');
       setDepth('search');
       onCancel();
-      // setIsNameNewRouteModalOpened(false);
-      // setIsOpenSelectNewRouteModal(false);
     },
     onError: async (error: any) => {
       await queryClient.invalidateQueries('getRoads');
@@ -42,57 +48,66 @@ const NameNewRouteModal = ({ item, onCancel, setDepth }: ModalProps) => {
   });
 
   return (
-    <Container>
-      <SubPathContainer>
-        <SubwaySimplePath
-          pathData={freshSubPathData}
-          arriveStationName={item.lastEndStation}
-          betweenPathMargin={24}
-        />
-      </SubPathContainer>
-      <FontText
-        value="새 경로 이름"
-        textSize="14px"
-        textWeight="Medium"
-        lineHeight="21px"
-        textColor={COLOR.BASIC_BLACK}
-      />
-
-      <InputBox>
-        <Input
-          placeholder="경로 이름을 입력하세요"
-          value={roadName}
-          onChangeText={(text) => setRoadName(text)}
-          inputMode="email"
-          maxLength={10}
-        ></Input>
-      </InputBox>
-
-      <TextLengthBox>
-        {isDuplicatedName ? (
-          <MessageContainer>
-            <Image source={iconPath.x_circle} style={{ width: 14, height: 14 }} />
-            <FontText
-              value={` 이미 존재하는 이름입니다`}
-              textSize="12px"
-              textWeight="Medium"
-              lineHeight="14px"
-              textColor={COLOR.LIGHT_RED}
-            />
-          </MessageContainer>
-        ) : (
-          <View></View>
-        )}
+    <KeyboardAwareScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      extraScrollHeight={Platform.OS === 'ios' ? 100 : 0}
+      enableOnAndroid={true}
+      keyboardShouldPersistTaps="handled"
+    >
+      <Container>
+        <SubPathContainer>
+          <SubwaySimplePath
+            pathData={freshSubPathData}
+            arriveStationName={item.lastEndStation}
+            betweenPathMargin={24}
+          />
+        </SubPathContainer>
         <FontText
-          value={`${roadName?.length ? roadName.length : 0}/10`}
-          textSize="12px"
-          textWeight="Regular"
-          textColor={COLOR.GRAY_999}
-          lineHeight="14px"
+          value="새 경로 이름"
+          textSize="14px"
+          textWeight="Medium"
+          lineHeight="21px"
+          textColor={COLOR.BASIC_BLACK}
         />
-      </TextLengthBox>
-
+        <InputBox>
+          <Input
+            placeholder="경로 이름을 입력하세요"
+            value={roadName}
+            onChangeText={(text) => setRoadName(text)}
+            inputMode="email"
+            maxLength={10}
+          ></Input>
+        </InputBox>
+        <TextLengthBox>
+          {isDuplicatedName ? (
+            <MessageContainer>
+              <Image source={iconPath.x_circle} style={{ width: 14, height: 14 }} />
+              <FontText
+                value={` 이미 존재하는 이름입니다`}
+                textSize="12px"
+                textWeight="Medium"
+                lineHeight="14px"
+                textColor={COLOR.LIGHT_RED}
+              />
+            </MessageContainer>
+          ) : (
+            <View></View>
+          )}
+          <FontText
+            value={`${roadName?.length ? roadName.length : 0}/10`}
+            textSize="12px"
+            textWeight="Regular"
+            textColor={COLOR.GRAY_999}
+            lineHeight="14px"
+          />
+        </TextLengthBox>
+      </Container>
       <BottomBtn
+        style={{
+          //FIXME: 안드로이드 애뮬레이터에서 키보드 올라올 때 marginBottom이 제대로 동작하지 않고 Input 아래에 붙음
+          marginBottom: isKeyboardVisible ? 0 : 41,
+          marginHorizontal: isKeyboardVisible ? -30 : 16,
+        }}
         onPress={() => {
           mutate({
             roadName: roadName,
@@ -111,7 +126,7 @@ const NameNewRouteModal = ({ item, onCancel, setDepth }: ModalProps) => {
           lineHeight="26px"
         />
       </BottomBtn>
-    </Container>
+    </KeyboardAwareScrollView>
   );
 };
 
@@ -123,9 +138,7 @@ const Container = styled.View`
   flex: 1;
 `;
 const SubPathContainer = styled.View`
-  margin-top: 48px;
-  margin-bottom: 40px;
-  margin-horizontal: 33px;
+  margin: 32px 33px 22px;
 `;
 const InputBox = styled.Pressable`
   padding-vertical: 12px;
@@ -135,7 +148,6 @@ const InputBox = styled.Pressable`
   background-color: ${COLOR.GRAY_F9};
 `;
 const TextLengthBox = styled.View`
-  align-items: flex-start;
   flex-direction: row;
   justify-content: space-between;
   flex: 1;
@@ -148,7 +160,6 @@ const BottomBtn = styled.Pressable`
   padding-vertical: 11px;
   border-radius: 5px;
   align-items: center;
-  bottom: 41px;
   ${({ disabled }) =>
     disabled ? `background-color : #dddddd` : `background-color : ${COLOR.BASIC_BLACK};`}
 `;
