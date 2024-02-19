10a3a3521373c18855792fd2b2158e883e2b7743
seoyeon <ryd10227@gmail.com>
2024-02-19 16:00:30 +0900

    회원가입 완료 시 자동로그인 구현

--- appFrontend/src/screens/signUpScreen/type.ts
+++ appFrontend/src/screens/signUpScreen/type.ts
@@ -2,6 +2,8 @@ export type SignUpStepType = 'email' | 'password' | 'nickname' | 'complete';
 export interface SightUpResponse {
   email: string;
   nickName: string;
+  accessToken: string;
+  refreshToken: string;
 }
 export interface SignUpParams {
   email: string;
