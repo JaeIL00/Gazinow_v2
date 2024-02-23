e75d0c8e1979d2bf3258fb20ef01a941e286c257
ShinJaeIl01 <ekvm555@naver.com>
2024-02-23 23:18:42 +0900

    웹 sentry 적용

--- webFrontend/src/global/apis/index.ts
+++ webFrontend/src/global/apis/index.ts
@@ -3,6 +3,7 @@ import apiUrls from "./url";
 import localStorageFunc from "@global/utils/localStorage";
 import { MemberReissue } from "./entity";
 import { STORAGE_ACCESS_KEY, STORAGE_REFRESH_KEY } from "@global/constants";
+import * as Sentry from "@sentry/react";
 
 // 로컬 개발 시 baseURL 주석처리하고 accessToken 직접 넣기
 const GaziAPI = axios.create({
@@ -21,6 +22,7 @@ GaziAPI.interceptors.response.use(
   async (error: AxiosError) => {
     // network error
     if (!error.response || error.response.status !== 401) {
+      Sentry.captureException(error);
       throw error;
     }
 
@@ -31,20 +33,23 @@ GaziAPI.interceptors.response.use(
       // return redirectLoginPage();
     }
 
-    const response = await axios.post<MemberReissue>(
-      apiUrls.member_reissue,
-      null,
-      {
-        baseURL: import.meta.env.VITE_BACKEND_URL,
-        headers: { Authorization: `Bearer ${refreshToken}` },
-      }
-    );
+    try {
+      const response = await axios.post<MemberReissue>(
+        apiUrls.member_reissue,
+        null,
+        {
+          baseURL: import.meta.env.VITE_BACKEND_URL,
+          headers: { Authorization: `Bearer ${refreshToken}` },
+        }
+      );
 
-    // 리다이렉트 유효성 분기점
-    if (response.status === 200) {
-      localStorageFunc.set(STORAGE_ACCESS_KEY, response.data.accessToken);
-      return GaziAPI(error.config || {});
-    } else {
+      // 리다이렉트 유효성 분기점
+      if (response.status === 200) {
+        localStorageFunc.set(STORAGE_ACCESS_KEY, response.data.accessToken);
+        return GaziAPI(error.config || {});
+      }
+    } catch (error) {
+      Sentry.captureException(error);
       localStorageFunc.removeAll();
       // FIXME: 리다이렉트를 어디로?
       // return redirectLoginPage();
--- webFrontend/src/main.tsx
+++ webFrontend/src/main.tsx
@@ -4,6 +4,21 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router/index.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RecoilRoot } from "recoil";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  release: "0.0.0",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+});
 
 const queryClient = new QueryClient();
 
