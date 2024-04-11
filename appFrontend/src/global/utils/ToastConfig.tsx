5ced0dca13289c4ddc75c17a840bfcdce24eae9c
seoyeon <ryd10227@gmail.com>
2024-04-11 22:50:32 +0900

    경로 저장 문구 수정

--- appFrontend/src/global/utils/ToastConfig.tsx
+++ appFrontend/src/global/utils/ToastConfig.tsx
@@ -7,7 +7,7 @@ const ToastConfig = {
   logout: () => <Toast text="로그아웃 되었어요" />,
   quit: () => <Toast text="탈퇴되었어요" />,
   deleteRoute: () => <Toast text="경로가 삭제되었어요" />,
-  saveRoute: () => <Toast text="경로가 저장했어요" />,
+  saveRoute: () => <Toast text="경로를 저장했어요" />,
 };
 
 export default ToastConfig;
