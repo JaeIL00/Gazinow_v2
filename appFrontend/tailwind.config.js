70d7d0147ed5949fef0a77d963c5a929473ddf71
seoyeon <ryd10227@gmail.com>
2024-04-15 20:00:46 +0900

    tailwind.config 기본 단위 rem->px로 변경하는 설정 추가

--- appFrontend/tailwind.config.js
+++ appFrontend/tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
+const px0_999 = { ...Array.from(Array(1000)).map((_, i) => `${i}px`) };
+
 module.exports = {
   content: [
     './src/App.{js,jsx,ts,tsx}',
@@ -6,7 +10,15 @@ module.exports = {
     './src/screens/**/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: px0_999,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      minWidth: px0_999,
+      minHeight: px0_999,
+      spacing: px0_999,
+      borderRadius: px0_999,
+    },
   },
   plugins: [],
 };
