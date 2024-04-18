69b820342369c214c2d8227f672a6b42fefe3e6b
ShinJaeIl01 <ekvm555@naver.com>
2024-04-18 21:04:08 +0900

    토스트 bottom 위치 수정

--- appFrontend/index.js
+++ appFrontend/index.js
@@ -7,8 +7,8 @@ import App from './src/App';
 import { name as appName } from './app.json';
 
 // TODO: 프로덕션 배포 시 주석처리하기
-// if (__DEV__) {
-//   require('./ReactotronConfig');
-// }
+if (__DEV__) {
+  require('./ReactotronConfig');
+}
 
 AppRegistry.registerComponent(appName, () => App);
