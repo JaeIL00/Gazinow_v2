fe92aa7dbcf5fd2af2b6a0f31f59b04b48c33922
seoyeon <ryd10227@gmail.com>
2024-07-27 09:35:26 +0900

    홈 인덱스에 화면 추가

--- appFrontend/src/screens/homeScreen/components/index.ts
+++ appFrontend/src/screens/homeScreen/components/index.ts
@@ -3,5 +3,6 @@ export { default as IssuesBanner } from './IssuesBanner';
 export { default as MyRoutes } from './MyRoutes';
 export { default as NonLoggedIn } from './NonLoggedIn';
 export { default as NoRoutes } from './NoRoutes';
+export { default as NotiHistory } from './NotiHistory';
 export { default as RouteItem } from './RouteItem';
 export { default as SwapStation } from './SwapStation';
