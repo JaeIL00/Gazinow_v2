f9bd076dc9be9caaac483e12541d7bc3c37b8ab5
seoyeon <ryd10227@gmail.com>
2024-07-27 09:35:10 +0900

    홈화면에 알림 아이콘 추가

--- appFrontend/src/screens/homeScreen/index.tsx
+++ appFrontend/src/screens/homeScreen/index.tsx
@@ -3,9 +3,15 @@ import { IssueCarrousel, SwapStation, MyRoutes } from './components';
 import { useEffect } from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import { useTryAuthorization } from './hooks';
+import IconBell from '@assets/icons/bell.svg';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useRootNavigation } from '@/navigation/RootNavigation';
+import { useHomeNavigation } from '@/navigation/HomeNavigation';
 
 const HomeScreen = () => {
   const { isVerifiedUser, tryAuthorization } = useTryAuthorization();
+  const rootNavigation = useRootNavigation();
+  const homeNavigation = useHomeNavigation();
 
   useEffect(() => {
     if (isVerifiedUser !== 'yet') {
@@ -19,6 +25,11 @@ const HomeScreen = () => {
     if (isVerifiedUser === 'yet') tryAuthorization();
   }, []);
 
+  const authStateHandler = () =>
+    isVerifiedUser === 'success auth'
+      ? homeNavigation.push('NotiHistory')
+      : rootNavigation.navigate('AuthStack', { screen: 'Landing' });
+
   return (
     <SafeAreaView className="flex-1 bg-gray-f9">
       <ScrollView
@@ -26,6 +37,14 @@ const HomeScreen = () => {
         showsVerticalScrollIndicator={false}
         scrollEnabled={isVerifiedUser === 'success auth'}
       >
+        <TouchableOpacity
+          onPress={authStateHandler}
+          hitSlop={20}
+          className="flex-row-reverse mt-15 ml-11"
+        >
+          {/* 아이콘 교체 */}
+          <IconBell />
+        </TouchableOpacity>
         <IssueCarrousel />
         <View className="h-16" />
         <SwapStation />
