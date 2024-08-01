e1ac0221d725762501ec788566f6e3caff9c2b24
seoyeon <ryd10227@gmail.com>
2024-08-01 16:32:09 +0900

    포그라운드/백그라운드 알림 수신 후 화면에 표시

--- appFrontend/src/App.tsx
+++ appFrontend/src/App.tsx
@@ -65,6 +65,40 @@ const App = (): JSX.Element => {
     requestPermission();
   }, []);
 
+  // 포그라운드 알림 수신
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      onMessageReceived(remoteMessage);
+    });
+    return unsubscribe;
+  }, []);
+
+  // 백그라운드 알림 수신 및 표시
+  messaging().setBackgroundMessageHandler(async (remoteMessage) => {});
+
+  // 포그라운드 알림 화면에 표시
+  const onMessageReceived = async (message: FirebaseMessagingTypes.RemoteMessage) => {
+    const { title, body } = message.notification!;
+
+    // 채널 생성
+    const channelId = await notifee.createChannel({
+      id: 'important',
+      name: 'Important Notifications',
+      importance: AndroidImportance.HIGH,
+    });
+
+    // 디바이스에 알림 표시
+    await notifee.displayNotification({
+      title,
+      body,
+      android: {
+        channelId,
+        importance: AndroidImportance.HIGH,
+        visibility: AndroidVisibility.PUBLIC,
+      },
+    });
+  };
+
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
