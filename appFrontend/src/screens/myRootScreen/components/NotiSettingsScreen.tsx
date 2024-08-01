6ec029d1efd7090c583259b66e59c2e7d2d03388
seoyeon <ryd10227@gmail.com>
2024-08-01 16:36:18 +0900

    알림 설정 화면 진입 시 권한 확인하는 라이브러리 변경

--- appFrontend/src/screens/myRootScreen/components/NotiSettingsScreen.tsx
+++ appFrontend/src/screens/myRootScreen/components/NotiSettingsScreen.tsx
@@ -1,51 +1,24 @@
-import React, { useEffect, useRef, useState } from 'react';
-import {
-  AppState,
-  AppStateStatus,
-  Platform,
-  SafeAreaView,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { AppState, AppStateStatus, SafeAreaView, TouchableOpacity, View } from 'react-native';
 import { COLOR } from '@/global/constants';
 import { FontText } from '@/global/ui';
 import IconLeftArrowHead from '@assets/icons/left_arrow_head.svg';
 import { useMyPageNavigation } from '@/navigation/MyPageNavigation';
 import RequestNotiOn from './RequestNotiOn';
 import NotiSettings from './NotiSettings';
-import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions';
+import notifee from '@notifee/react-native';
 
 const NotiSettingsScreen = () => {
   const myPageNavigation = useMyPageNavigation();
 
   const [isNotiPermissionOn, setIsNotiPermissionOn] = useState<boolean>(false);
 
-  // const appState = useRef(AppState.currentState);
-
   const checkNotificationPermission = async () => {
-    let result;
-
-    // if (Platform.OS === 'ios') {
-    //   result = await check(PERMISSIONS.IOS.);
-    // } else {
-    result = await check(PERMISSIONS.ANDROID.POST_NOTIFICATIONS);
-    // }
-    switch (result) {
-      case RESULTS.UNAVAILABLE:
-        setIsNotiPermissionOn(false);
-        break;
-      case RESULTS.DENIED:
-        setIsNotiPermissionOn(false);
-        break;
-      case RESULTS.LIMITED:
-        setIsNotiPermissionOn(true);
-        break;
-      case RESULTS.GRANTED:
-        setIsNotiPermissionOn(true);
-        break;
-      case RESULTS.BLOCKED:
-        setIsNotiPermissionOn(false);
-        break;
+    const settings = await notifee.requestPermission();
+    if (settings.authorizationStatus) {
+      setIsNotiPermissionOn(true);
+    } else {
+      setIsNotiPermissionOn(false);
     }
   };
 
