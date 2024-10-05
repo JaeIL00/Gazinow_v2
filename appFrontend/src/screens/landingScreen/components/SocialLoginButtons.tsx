7407fcf9eb2cb1cf1f938662af6a6ab6e7185aab
seoyeon <ryd10227@gmail.com>
2024-10-06 05:29:07 +0900

    소셜로그인 버튼 리팩토링

--- appFrontend/src/screens/landingScreen/components/SocialLoginButtons.tsx
+++ appFrontend/src/screens/landingScreen/components/SocialLoginButtons.tsx
@@ -1,69 +1,72 @@
+import React from 'react';
 import { View, Platform, TouchableOpacity, Linking } from 'react-native';
+import { FontText } from '@/global/ui';
+import { API_BASE_URL } from '@env';
+
 import LoginNaver from '@assets/icons/login_naver.svg';
 import LoginApple from '@assets/icons/login_apple.svg';
 import LoginGoogle from '@assets/icons/login_google.svg';
-import { FontText } from '@/global/ui';
-import { ReactNode } from 'react';
-import cn from 'classname';
-import { API_BASE_URL } from '@env';
+import { SvgProps } from 'react-native-svg';
 
-export type SocialLoginType = 'naver' | 'google' | 'kakao' | 'apple';
+const SOCIAL_LOGIN_BUTTONS = [
+  {
+    type: 'naver',
+    icon: LoginNaver,
+    label: '네이버로 계속하기',
+    bgColor: 'bg-[#03C75A]',
+    textColor: 'text-white',
+  },
+  {
+    type: 'apple',
+    icon: LoginApple,
+    label: 'Apple로 계속하기',
+    bgColor: 'bg-black',
+    textColor: 'text-white',
+  },
+  {
+    type: 'google',
+    icon: LoginGoogle,
+    label: '구글로 계속하기',
+    bgColor: 'bg-white',
+    textColor: 'text-black',
+  },
+];
 
-interface SocialLoginButtonProps {
-  icon: ReactNode;
+interface SocialLoginButtonsProps {
+  icon: React.FC<SvgProps>;
   label: string;
   bgColor: string;
-  textColor?: string;
-  socialLoginTypeProps: SocialLoginType;
+  textColor: string;
+  type: string;
 }
 
 const SocialLoginButton = ({
-  icon,
+  icon: Icon,
   label,
   bgColor,
   textColor,
-  socialLoginTypeProps,
-}: SocialLoginButtonProps) => (
+  type,
+}: SocialLoginButtonsProps) => (
   <TouchableOpacity
     className={`flex-row items-center px-16 py-6 mb-12 rounded-30 ${bgColor}`}
-    onPress={() =>
-      Linking.openURL(`${API_BASE_URL}/api/v1/oauth/login?socialLoginType=${socialLoginTypeProps}`)
-    }
+    onPress={() => Linking.openURL(`${API_BASE_URL}/api/v1/oauth/login?socialLoginType=${type}`)}
   >
-    {icon}
+    <Icon />
     <View className="flex-grow items-center pr-34">
-      <FontText className={`${textColor}`} value={label} textSize="14px" textWeight="Medium" />
+      <FontText className={textColor} value={label} textSize="14px" textWeight="Medium" />
     </View>
   </TouchableOpacity>
 );
 
-const SocialLoginButtons = () => {
-  return (
-    <View className={cn('mx-30 mb-18 items-center', { 'mb-8': Platform.OS !== 'ios' })}>
-      <SocialLoginButton
-        icon={<LoginNaver />}
-        label="네이버로 계속하기"
-        bgColor="bg-[#03C75A]"
-        textColor="text-white"
-        socialLoginTypeProps="naver"
-      />
-      {Platform.OS === 'ios' && (
-        <SocialLoginButton
-          icon={<LoginApple />}
-          label="Apple로 계속하기"
-          bgColor="bg-black"
-          textColor="text-white"
-          socialLoginTypeProps="apple"
-        />
-      )}
-      <SocialLoginButton
-        icon={<LoginGoogle />}
-        label="구글로 계속하기"
-        bgColor="bg-white"
-        socialLoginTypeProps="google"
-      />
-    </View>
-  );
-};
+const SocialLoginButtons = () => (
+  <View className="mx-30 mb-18 items-center">
+    {SOCIAL_LOGIN_BUTTONS.map(
+      (button) =>
+        (Platform.OS === 'ios' || button.type !== 'apple') && (
+          <SocialLoginButton key={button.type} {...button} />
+        ),
+    )}
+  </View>
+);
 
 export default SocialLoginButtons;
