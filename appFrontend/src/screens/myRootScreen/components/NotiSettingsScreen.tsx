d1370b7d43107df43f181233123d93da4facb43b
seoyeon <ryd10227@gmail.com>
2024-10-10 04:24:12 +0900

    알림 설정 화면 '완료' 버튼 제거

--- appFrontend/src/screens/myRootScreen/components/NotiSettingsScreen.tsx
+++ appFrontend/src/screens/myRootScreen/components/NotiSettingsScreen.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { AppState, AppStateStatus, SafeAreaView, TouchableOpacity, View } from 'react-native';
-import { COLOR } from '@/global/constants';
 import { FontText } from '@/global/ui';
 import IconLeftArrowHead from '@assets/icons/left_arrow_head.svg';
 import { useMyPageNavigation } from '@/navigation/MyPageNavigation';
@@ -33,8 +32,6 @@ const NotiSettingsScreen = () => {
     };
   }, [checkNotificationPermission]);
 
-  const submitNotificationSettings = () => {};
-
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="flex-row px-16 items-center justify-between">
@@ -44,10 +41,6 @@ const NotiSettingsScreen = () => {
         >
           <IconLeftArrowHead color="#3F3F46" className="ml-6 mr-21" />
           <FontText value="알림 설정" textSize="18px" lineHeight={23} textWeight="Medium" />
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => submitNotificationSettings()}>
-          <FontText value="완료" textSize="16px" textColor={COLOR.GRAY_999} textWeight="SemiBold" />
         </TouchableOpacity>
       </View>
       {isNotiPermissionOn ? <NotiSettings /> : <RequestNotiOn />}
