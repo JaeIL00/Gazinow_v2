4a2ec4255bdd69e1869a2898322cb9208fa005c3
seoyeon <ryd10227@gmail.com>
2024-11-07 16:31:25 +0900

    ios 백그라운드 알림 수신 처리

--- appFrontend/index.js
+++ appFrontend/index.js
@@ -5,11 +5,17 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import messaging from '@react-native-firebase/messaging';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // TODO: 프로덕션 배포 시 주석처리하기
 // if (__DEV__) {
 //   require('./ReactotronConfig');
 // }
+
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  await AsyncStorage.setItem('pushNotiParams', remoteMessage.data?.path);
+});
 
 function HeadlessCheck({ isHeadless }) {
   if (isHeadless) {
@@ -20,5 +26,3 @@ function HeadlessCheck({ isHeadless }) {
 }
 
 AppRegistry.registerComponent(appName, () => HeadlessCheck);
-
-// AppRegistry.registerComponent(appName, () => App);
