827787385329f3ef07708636c4e7947756c0fa78
seoyeon <ryd10227@gmail.com>
2024-11-23 22:18:49 +0900

    경로 검색 결과 화면 ui 수정, 테일윈드 마이그레이션

--- appFrontend/src/screens/searchPathResultScreen/index.tsx
+++ appFrontend/src/screens/searchPathResultScreen/index.tsx
@@ -1,23 +1,20 @@
 import { Pressable, SafeAreaView, ScrollView, TouchableOpacity, View } from 'react-native';
-
-import { FontText, Space } from '@/global/ui';
+import { FontText } from '@/global/ui';
 import { COLOR } from '@/global/constants';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 import { SubwaySimplePath } from '@/global/components';
 import { useGetSearchPaths } from '@/global/apis/hooks';
-import { useAppDispatch, useAppSelect } from '@/store';
-import { StationDataTypes, getIssueId } from '@/store/modules';
+import { useAppSelect } from '@/store';
+import { StationDataTypes } from '@/store/modules';
 import React from 'react';
 import { useHomeNavigation } from '@/navigation/HomeNavigation';
 import SwapStation from './components/SwapStation';
 import IconRightArrowHead from '@assets/icons/right_arrow_head.svg';
 import IconLeftArrowHead from '@assets/icons/left_arrow_head.svg';
-import IssueKeywordIcon from '@/global/components/IssueKeywordIcon';
-import { subwayLineColor } from '@/global/utils';
-import { useRootNavigation } from '@/navigation/RootNavigation';
 import { Path } from '@/global/apis/entity';
 import LoadingCircle from '@/global/components/animations/LoadingCircle';
+import { IssuesBanner } from '../homeScreen/components';
 
 dayjs.locale('ko');
 
@@ -28,9 +25,7 @@ export interface SelectedStationTypes {
 
 const SearchPathResultScreen = () => {
   const homeNavigation = useHomeNavigation();
-  const rootNavigation = useRootNavigation();
   const selectedStationRedux = useAppSelect(({ subwaySearch }) => subwaySearch.selectedStation);
-  const dispatch = useAppDispatch();
 
   const { data, isLoading } = useGetSearchPaths({
     params: {
@@ -55,137 +50,59 @@ const SearchPathResultScreen = () => {
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLOR.WHITE }}>
-      <View style={{ backgroundColor: COLOR.GRAY_F2, flex: 1 }}>
-        <View
-          style={{
-            backgroundColor: COLOR.WHITE,
-            flexDirection: 'row',
-            paddingTop: 16,
-            paddingRight: 16,
-            paddingBottom: 15,
-            paddingLeft: 22,
-            marginBottom: 16,
-          }}
-        >
-          <View style={{ marginTop: 4, marginRight: 16 }}>
-            <TouchableOpacity onPress={() => homeNavigation.goBack()}>
-              <IconLeftArrowHead color="#3F3F46" />
-            </TouchableOpacity>
-          </View>
-          <SwapStation selectedStation={selectedStationRedux} />
-        </View>
-
-        <View
-          style={{
-            backgroundColor: COLOR.WHITE,
-            paddingHorizontal: 16,
-            paddingVertical: 14,
-            borderBottomColor: COLOR.GRAY_EB,
-            borderBottomWidth: 1,
-          }}
-        >
-          <FontText
-            text={'오늘 ' + dayjs().format('A HH시 mm분') + ' 기준'}
-            className="text-brown-54f"
-          />
-        </View>
-        <ScrollView style={{ backgroundColor: COLOR.WHITE }}>
-          {isLoading && (
-            <View style={{ marginTop: 100, alignItems: 'center' }}>
-              <LoadingCircle width={40} height={40} />
-            </View>
-          )}
-          {data &&
-            data.paths.map((item, idx) => (
-              <Pressable
-                key={item.firstStartStation + item.subPaths.length + idx}
-                style={({ pressed }) => ({
-                  backgroundColor: pressed ? COLOR.GRAY_E5 : 'transparent',
-                  paddingHorizontal: 18,
-                  paddingBottom: 24,
-                  paddingTop: 20,
-                  borderBottomColor: data.paths.length - 1 !== idx ? COLOR.GRAY_EB : 'none',
-                  borderBottomWidth: data.paths.length - 1 !== idx ? 1 : 0,
-                })}
-                onPress={() =>
-                  homeNavigation.push('SubwayPathDetail', {
-                    state: item,
-                  })
-                }
-              >
-                <View style={{ marginBottom: 16 }}>
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                      justifyContent: 'space-between',
-                    }}
-                  >
-                    <FontText
-                      text="평균 소요시간"
-                      className="text-11 text-gray-999"
-                      fontWeight="600"
-                    />
-                    <View className="flex-row items-center">
-                      <FontText text="세부정보" className="text-13 text-gray-999" />
-                      <Space width={4} />
-                      <IconRightArrowHead color={COLOR.GRAY_999} />
-                    </View>
-                  </View>
-                  <View style={{ height: 4 }} />
-                  <FontText text={pathTime(item)} className="text-20" fontWeight="600" />
-                </View>
-
-                {/* 지하철 경로 UI */}
-                <SubwaySimplePath
-                  pathData={item.subPaths}
-                  arriveStationName={item.lastEndStation}
-                  betweenPathMargin={24}
-                />
-
-                <View>
-                  {item.subPaths.map((linePath, idx) => {
-                    return (
-                      <React.Fragment key={linePath.sectionTime + 'subPath' + idx}>
-                        {linePath.issueSummary &&
-                          linePath.issueSummary.map(({ keyword, title, id }, innerIdx) => {
-                            if (innerIdx > 2) return <></>;
-                            return (
-                              <TouchableOpacity
-                                key={id + title + innerIdx + 'idx' + idx}
-                                className="flex-row items-center justify-between px-12 py-8 mb-8 overflow-hidden rounded-full border-gray-beb border-1"
-                                onPress={() => {
-                                  dispatch(getIssueId(id));
-                                  rootNavigation.navigate('IssueStack', {
-                                    screen: 'IssueDetail',
-                                  });
-                                }}
-                              >
-                                <IssueKeywordIcon
-                                  width={24}
-                                  height={24}
-                                  keyword={keyword}
-                                  color={subwayLineColor(linePath.stationCode)}
-                                />
-                                <FontText
-                                  text={title}
-                                  className="flex-1 ml-10 mr-10 text-13"
-                                  fontWeight="600"
-                                  numberOfLines={1}
-                                />
-                                <IconRightArrowHead width={8} height={8} color={COLOR.GRAY_999} />
-                              </TouchableOpacity>
-                            );
-                          })}
-                      </React.Fragment>
-                    );
-                  })}
-                </View>
-              </Pressable>
-            ))}
-        </ScrollView>
+    <SafeAreaView className="flex-1 bg-gray-f2">
+      <View className="flex-row p-16 mb-16 bg-white pb-15 pl-22">
+        <TouchableOpacity className="mt-4 mr-16" onPress={() => homeNavigation.goBack()}>
+          <IconLeftArrowHead color="#3F3F46" />
+        </TouchableOpacity>
+        <SwapStation selectedStation={selectedStationRedux} />
       </View>
+
+      <View className="px-16 bg-white py-14 border-b-1 border-gray-beb">
+        <FontText
+          text={'오늘 ' + dayjs().format('A HH시 mm분') + ' 기준'}
+          className="text-brown-54f"
+        />
+      </View>
+      <ScrollView style={isLoading && { backgroundColor: 'white' }}>
+        {isLoading && (
+          <View className="items-center mt-100">
+            <LoadingCircle width={40} height={40} />
+          </View>
+        )}
+        {data &&
+          data.paths.map((item, idx) => (
+            <Pressable
+              key={item.firstStartStation + item.subPaths.length + idx}
+              style={({ pressed }) => ({
+                backgroundColor: pressed ? COLOR.GRAY_E5 : 'white',
+                paddingHorizontal: 18,
+                paddingBottom: 24,
+                paddingTop: 20,
+                borderBottomColor: COLOR.GRAY_EB,
+                borderBottomWidth: 1,
+              })}
+              onPress={() => homeNavigation.push('SubwayPathDetail', { state: item })}
+            >
+              <View className="flex-row items-center justify-between">
+                <FontText text="평균 소요시간" className="text-11 text-gray-999" fontWeight="600" />
+                <View className="flex-row items-center">
+                  <FontText text="세부정보" className="mr-4 text-13 text-gray-999" />
+                  <IconRightArrowHead color={COLOR.GRAY_999} />
+                </View>
+              </View>
+              <FontText text={pathTime(item)} className="mt-4 mb-16 text-20" fontWeight="600" />
+
+              {/* 지하철 경로 UI */}
+              <SubwaySimplePath
+                pathData={item.subPaths}
+                arriveStationName={item.lastEndStation}
+                betweenPathMargin={24}
+              />
+              <IssuesBanner subPaths={item.subPaths} />
+            </Pressable>
+          ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
