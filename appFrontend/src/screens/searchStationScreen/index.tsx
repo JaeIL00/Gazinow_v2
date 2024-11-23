f91e13d8a142562a8d57a208baa403c1e9fdb366
seoyeon <ryd10227@gmail.com>
2024-11-24 02:41:45 +0900

    검색창 텍스트 입력 위치 수정

--- appFrontend/src/screens/searchStationScreen/index.tsx
+++ appFrontend/src/screens/searchStationScreen/index.tsx
@@ -84,7 +84,6 @@ const SearchStationScreen = () => {
         <TouchableOpacity hitSlop={20} onPress={() => navigation.goBack()}>
           <IconLeftArrow />
         </TouchableOpacity>
-        <Space width={16} />
         <Input
           value={searchTextValue}
           placeholder={`${stationType}을 검색해보세요`}
@@ -92,7 +91,7 @@ const SearchStationScreen = () => {
           inputMode="search"
           onChangeText={changeSearchText}
           autoFocus
-          className="ml-[18.25px] mr-[31.2px] flex-1 h-36"
+          className="ml-16 mr-[31.2px] flex-1 h-36"
         />
         <TouchableOpacity hitSlop={20} onPress={deleteInputText}>
           <IconXCircleFill />
