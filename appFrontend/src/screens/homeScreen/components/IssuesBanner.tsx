8f12e53c8b1e446a96f956c34b12852869308506
seoyeon <ryd10227@gmail.com>
2024-12-10 05:10:41 +0900

    이슈 배너 중복 제거하고 하단 마진값 적용

--- appFrontend/src/screens/homeScreen/components/IssuesBanner.tsx
+++ appFrontend/src/screens/homeScreen/components/IssuesBanner.tsx
@@ -1,47 +1,54 @@
 import React from 'react';
 import { FontText } from '@/global/ui';
-import { IssueSummary, SubPath } from '@/global/apis/entity';
+import { SubPath } from '@/global/apis/entity';
 import MoreBtn from '@/assets/icons/moreBtn.svg';
-import { subwayLineColor } from '@/global/utils';
 import { useAppDispatch } from '@/store';
 import { getIssueId } from '@/store/modules';
 import { useRootNavigation } from '@/navigation/RootNavigation';
-import IssueKeywordIcon from '@/global/components/IssueKeywordIcon';
 import { TouchableOpacity } from 'react-native';
+import cn from 'classname';
 
 interface IssuesBannerProps {
   subPaths: SubPath[];
 }
 
 const IssuesBanner = ({ subPaths }: IssuesBannerProps) => {
+  const issues = subPaths.filter((subPath) => !!subPath.issueSummary.length);
+  if (issues.length < 1) return null;
+
   const dispatch = useAppDispatch();
   const navigation = useRootNavigation();
 
-  const issues = subPaths.filter((subPath) => !!subPath.issueSummary.length);
+  const allIssueSummary = issues.flatMap((issue) => issue.issueSummary);
 
-  if (issues.length < 1) return null;
+  // title 기준으로 중복 제거
+  const uniqueIssueSummary = Array.from(
+    new Map(allIssueSummary.map((issue) => [issue.title, issue])).values(),
+  );
+
   return (
     <>
-      {issues.map(({ issueSummary }, index: number) =>
-        issueSummary.map((issue: IssueSummary, issueIndex: number) => (
-          <TouchableOpacity
-            key={`${index}-${issueIndex}`}
-            onPress={() => {
-              dispatch(getIssueId(issue.id));
-              navigation.navigate('IssueStack', { screen: 'IssueDetail' });
-            }}
-            className="flex-row items-center justify-between px-16 py-12 overflow-hidden bg-white rounded-full border-gray-beb border-1"
-          >
-            <FontText
-              className="mr-10 text-14 leading-21"
-              text={issue.title}
-              fontWeight="600"
-              numberOfLines={1}
-            />
-            <MoreBtn />
-          </TouchableOpacity>
-        )),
-      )}
+      {uniqueIssueSummary.map((issue, index: number) => (
+        <TouchableOpacity
+          key={`${index}-${issue}`}
+          onPress={() => {
+            dispatch(getIssueId(issue.id));
+            navigation.navigate('IssueStack', { screen: 'IssueDetail' });
+          }}
+          className={cn(
+            'flex-row items-center justify-between px-16 py-12 overflow-hidden bg-white rounded-full border-gray-beb border-1',
+            { 'mb-8': index !== uniqueIssueSummary.length - 1 },
+          )}
+        >
+          <FontText
+            className="mr-10 text-14 leading-21"
+            text={issue.title}
+            fontWeight="600"
+            numberOfLines={1}
+          />
+          <MoreBtn />
+        </TouchableOpacity>
+      ))}
     </>
   );
 };
