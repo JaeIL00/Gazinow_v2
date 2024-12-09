c507ab66d17deef877145a109859867d87be2614
seoyeon <ryd10227@gmail.com>
2024-12-09 17:51:01 +0900

    홈화면 ui 버그 픽스

--- appFrontend/src/screens/homeScreen/components/NoRoutes.tsx
+++ appFrontend/src/screens/homeScreen/components/NoRoutes.tsx
@@ -1,11 +1,14 @@
+import { View } from 'react-native';
 import { FontText } from '@/global/ui';
 
 const NoRoutes = () => (
-  <FontText
-    text="저장한 경로가 없어요"
-    className="text-center text-16 py-27 border-t-1 border-gray-beb text-gray-999"
-    fontWeight="500"
-  />
+  <View className="border-t-1 border-gray-beb">
+    <FontText
+      text="저장한 경로가 없어요"
+      className="text-center text-16 py-27 text-gray-999"
+      fontWeight="500"
+    />
+  </View>
 );
 
 export default NoRoutes;
