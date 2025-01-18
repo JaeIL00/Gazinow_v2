89e3db15773da3c329f15e97c9675ce7aabfb958
seoyeon <ryd10227@gmail.com>
2025-01-18 17:53:00 +0900

    이슈 댓글 등록, 조회, 좋아요 등 api func/hook/entity 추가

--- appFrontend/src/screens/issueDetailScreen/api/func.ts
+++ appFrontend/src/screens/issueDetailScreen/api/func.ts
@@ -1,10 +1,11 @@
 import { AxiosError } from 'axios';
-import { IssueGet } from './entity';
+import { CommentReportType, IssueGet, PostCommentType } from './entity';
 import * as Sentry from '@sentry/react';
 import { authServiceAPI, publicServiceAPI } from '@/global/apis';
+import { AllComments } from '@/global/apis/entity';
 
 /**
- * 상세 이슈 조회
+ * 이슈 본문 조회
  */
 export const getIssueDetail = async ({
   params,
@@ -28,7 +29,7 @@ export const getIssueDetail = async ({
   } catch (err) {
     const error = err as AxiosError;
     Sentry.captureException({
-      target: '상세 이슈 조회',
+      target: '이슈 본문 조회',
       input: { params, isVerifiedUser, request: error.request },
       output: { status: error.response?.status, error: error.message, response: error.response },
     });
@@ -37,16 +38,15 @@ export const getIssueDetail = async ({
 };
 
 /**
- * 도움돼요 추가
+ * 이슈 본문 도움돼요 추가
  */
 export const postLike = async (issueId: number) => {
   try {
-    const res = await authServiceAPI.post(`/api/v1/like?issueId=${issueId}`);
-    return res.data.data;
+    await authServiceAPI.post(`/api/v1/like?issueId=${issueId}`);
   } catch (err) {
     const error = err as AxiosError;
     Sentry.captureException({
-      target: '도움돼요 추가',
+      target: '이슈 본문 도움돼요 추가',
       input: { issueId, request: error.request },
       output: { status: error.response?.status, error: error.message, response: error.response },
     });
@@ -55,17 +55,136 @@ export const postLike = async (issueId: number) => {
 };
 
 /**
- * 도움돼요 삭제
+ * 이슈 본문 도움돼요 삭제
  */
 export const deletePostLike = async (issueId: number) => {
   try {
-    const res = await authServiceAPI.delete(`/api/v1/like?issueId=${issueId}`);
-    return res.data.data;
+    await authServiceAPI.delete(`/api/v1/like?issueId=${issueId}`);
   } catch (err) {
     const error = err as AxiosError;
     Sentry.captureException({
-      target: '도움돼요 삭제',
+      target: '이슈 본문 도움돼요 삭제',
       input: { issueId, request: error.request },
+      output: { status: error.response?.status, error: error.message, response: error.response },
+    });
+    throw error;
+  }
+};
+
+/**
+ * 이슈에 달린 댓글 조회
+ */
+export const getCommentsOnAIssueFetch = async ({
+  isVerifiedUser,
+  params,
+}: {
+  isVerifiedUser: 'success auth' | 'fail auth' | 'yet';
+  params: { issueId: number; page: number };
+}) => {
+  try {
+    if (isVerifiedUser === 'success auth') {
+      const res = await authServiceAPI.get<{ data: AllComments }>(
+        `/api/v1/comments/${params.issueId}`,
+        { params },
+      );
+      return res.data.data;
+    } else {
+      const res = await publicServiceAPI.get<{ data: AllComments }>(
+        `/api/v1/comments/${params.issueId}`,
+        { params },
+      );
+      return res.data.data;
+    }
+  } catch (err) {
+    const error = err as AxiosError;
+    Sentry.captureException({
+      target: '이슈 댓글 조회',
+      input: { isVerifiedUser, issueId: params.issueId, request: error.request },
+      output: { status: error.response?.status, error: error.message, response: error.response },
+    });
+    throw error;
+  }
+};
+
+/**
+ * 이슈 댓글 등록
+ */
+export const postComment = async (body: PostCommentType) => {
+  try {
+    await authServiceAPI.post(`/api/v1/comments`, body);
+  } catch (err) {
+    const error = err as AxiosError;
+    Sentry.captureException({
+      target: '이슈 댓글 등록',
+      input: { body, request: error.request },
+      output: { status: error.response?.status, error: error.message, response: error.response },
+    });
+    throw error;
+  }
+};
+
+/**
+ * 이슈 댓글 삭제
+ */
+export const deleteComment = async (issueCommentId: number) => {
+  try {
+    await authServiceAPI.delete(`/api/v1/comments/${issueCommentId}`);
+  } catch (err) {
+    const error = err as AxiosError;
+    Sentry.captureException({
+      target: '이슈 댓글 삭제',
+      input: { issueCommentId, request: error.request },
+      output: { status: error.response?.status, error: error.message, response: error.response },
+    });
+    throw error;
+  }
+};
+
+/**
+ * 이슈 댓글 좋아요 추가
+ */
+export const postCommentLike = async (issueCommentId: number) => {
+  try {
+    await authServiceAPI.post(`/api/v1/comments/likes`, { issueCommentId });
+  } catch (err) {
+    const error = err as AxiosError;
+    Sentry.captureException({
+      target: '이슈 댓글 좋아요 추가',
+      input: { issueCommentId, request: error.request },
+      output: { status: error.response?.status, error: error.message, response: error.response },
+    });
+    throw error;
+  }
+};
+
+/**
+ * 이슈 댓글 좋아요 삭제
+ */
+export const deleteCommentLike = async (issueCommentId: number) => {
+  try {
+    await authServiceAPI.delete(`/api/v1/comments/likes/${issueCommentId}`);
+  } catch (err) {
+    const error = err as AxiosError;
+    Sentry.captureException({
+      target: '이슈 댓글 좋아요 삭제',
+      input: { issueCommentId, request: error.request },
+      output: { status: error.response?.status, error: error.message, response: error.response },
+    });
+    throw error;
+  }
+};
+
+/**
+ * 이슈 댓글 신고
+ */
+export const reportComment = async (data: CommentReportType) => {
+  try {
+    await authServiceAPI.post('/api/v1/report', data);
+  } catch (err) {
+    const error = err as AxiosError;
+    Sentry.captureException({
+      target: '이슈 댓글 신고',
+      input: { data, request: error.request },
       output: { status: error.response?.status, error: error.message, response: error.response },
     });
     throw error;
