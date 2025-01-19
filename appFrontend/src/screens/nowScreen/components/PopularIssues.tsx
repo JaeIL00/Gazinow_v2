879c45c455719c8c7afaec793dcf798d6569c2af
seoyeon <ryd10227@gmail.com>
2025-01-19 18:19:46 +0900

    나우탭 디자인과 구조 변경. FlatList 헤더에 '지금 인기 이슈' 컴포넌트 적용

---
+++ appFrontend/src/screens/nowScreen/components/PopularIssues.tsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import { Pressable, TouchableOpacity, View } from 'react-native';
+import { FontText } from '@/global/ui';
+import { getIssueId } from '@/store/modules';
+import { useAppDispatch } from '@/store';
+import { useRootNavigation } from '@/navigation/RootNavigation';
+import dayjs from 'dayjs';
+import cn from 'classname';
+import IconHeart from '@/assets/icons/icon-heart-mono.svg';
+import { IssueContent } from '@/global/apis/entity';
+
+interface PopularIssuesProps {
+  popularIssues: IssueContent[];
+}
+
+const PopularIssues = ({ popularIssues }: PopularIssuesProps) => {
+  const dispatch = useAppDispatch();
+  const navigation = useRootNavigation();
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  return (
+    <View>
+      <View className="px-16 pt-24 pb-28">
+        <FontText text="지금 인기 이슈" className="text-20 leading-25" fontWeight="600" />
+      </View>
+
+      <View className="px-16 pb-36">
+        <View className="rounded-16 py-20 px-16 bg-[#F9FAFB]">
+          {popularIssues?.map(({ id, title, expireDate, likeCount }, index) => {
+            const isExpired = dayjs().isAfter(dayjs(expireDate));
+            if (!isExpanded && index !== 0) return null;
+            return (
+              <Pressable
+                style={({ pressed }) => ({
+                  //TODO: Pressed 디자인 나오면
+                  //   backgroundColor: pressed ? COLOR.GRAY_E5 : 'transparent',
+                  marginTop: index === 0 ? 0 : 36,
+                })}
+                onPress={() => {
+                  dispatch(getIssueId(id));
+                  navigation.navigate('IssueStack', { screen: 'IssueDetail' });
+                }}
+                key={`${id}_${index}`}
+              >
+                <View className="flex-row space-x-10">
+                  <FontText text={`${index + 1}`} className="w-12" fontWeight="700" />
+                  <View className="space-y-12">
+                    <FontText text={title} fontWeight="500" numberOfLines={2} />
+                    <View className="flex-row items-center space-x-8">
+                      <View className="flex-row items-center space-x-4">
+                        <View
+                          className={cn('w-6 h-6 rounded-full bg-light-red', {
+                            'bg-gray-999': isExpired,
+                          })}
+                        />
+                        <FontText
+                          text={isExpired ? '종료' : '진행중'}
+                          className={cn('text-13 leading-19 text-light-red ', {
+                            'text-gray-999': isExpired,
+                          })}
+                        />
+                      </View>
+                      <View className="w-1 h-10 bg-[#D9D9D9]" />
+                      <View className="flex-row items-center space-x-4">
+                        <IconHeart color="#D1D6DB" width={18} height={18} />
+                        <FontText
+                          text={'' + likeCount}
+                          className="text-13 leading-19 text-gray-999"
+                        />
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              </Pressable>
+            );
+          })}
+          <View className="flex-row-reverse">
+            <TouchableOpacity onPress={() => setIsExpanded((prev) => !prev)} hitSlop={30}>
+              <FontText
+                text={isExpanded ? '접기' : '펼쳐보기'}
+                className="text-right text-13 leading-19 text-gray-999"
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <View className="h-16 bg-gray-9f9" />
+    </View>
+  );
+};
+export default PopularIssues;
--- appFrontend/src/screens/nowScreen/index.tsx
+++ appFrontend/src/screens/nowScreen/index.tsx
@@ -1,34 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { COLOR } from '@/global/constants';
-import { useQueryClient } from 'react-query';
-import { IssueContainer, LaneButtons } from './components';
-import { FreshSubwayLineName, IssueContent, NowScreenCapsules } from '@/global/apis/entity';
+import React, { useMemo, useState } from 'react';
+import { SingleIssueContainer, LaneButtons, PopularIssues } from './components';
+import { FreshSubwayLineName, NowScreenCapsules } from '@/global/apis/entity';
 import { FlatList, RefreshControl, SafeAreaView, View } from 'react-native';
 import {
   useGetAllIssuesQuery,
-  useGetPopularIssuesQuery,
   useGetIssuesByLaneQuery,
+  useGetPopularIssuesQuery,
 } from '@/global/apis/hooks';
-import { FontText, Space } from '@/global/ui';
+import { FontText } from '@/global/ui';
 import { subwayReturnLineName } from '@/global/utils/subwayLine';
 import LoadingCircle from '@/global/components/animations/LoadingCircle';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-
-dayjs.locale('ko');
-dayjs.extend(relativeTime);
 
 const NowScreen = () => {
-  const queryClient = useQueryClient();
+  const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
   const [activeButton, setActiveButton] = useState<NowScreenCapsules>('전체');
-  const [isRefresh, setRefresh] = useState<boolean>(false);
-  const [issuesList, setIssuesList] = useState<IssueContent[]>([]);
-  const [layoutHeight, setLayoutHeight] = useState<number>(0);
-  const [btnTitleHeight, setBtnTitleHeight] = useState<number>(0);
-  const flatListRef = useRef<FlatList>(null);
 
-  const { popularIssues, popularIssuesRefetch, isPopularIssuesLoading } =
-    useGetPopularIssuesQuery();
+  const { popularIssues, popularIssuesRefetch } = useGetPopularIssuesQuery();
 
   const {
     allIssues,
@@ -41,36 +28,31 @@ const NowScreen = () => {
   const { laneIssues, laneIssuesRefetch, laneIssuesHasNextPage, fetchLaneIssuesNextPage } =
     useGetIssuesByLaneQuery(subwayReturnLineName(activeButton as FreshSubwayLineName)!);
 
-  useEffect(() => {
-    queryClient.invalidateQueries(['getAllIssues', 'getIssuesByLane', 'getPopularIssues']);
-    setIssuesList([]);
+  // 선택된 캡슐에 따른 데이터 렌더링
+  const flattenedData = useMemo(
+    () =>
+      activeButton === '전체'
+        ? allIssues?.pages.flatMap((page) => page.content) ?? []
+        : laneIssues?.pages.flatMap((page) => page.content) ?? [],
+    [activeButton, allIssues, laneIssues],
+  );
 
-    if (isRefresh) {
-      const refetchPopularIssues = async () => {
-        await popularIssuesRefetch();
-        setRefresh(false);
-      };
-      refetchPopularIssues();
+  const onRefreshHandler = async () => {
+    setIsRefreshing(true);
+    try {
+      await Promise.all([
+        popularIssuesRefetch(),
+        activeButton === '전체' ? allIssuesRefetch() : laneIssuesRefetch(),
+      ]);
+    } catch (error) {
+      // 에러 처리
+    } finally {
+      setIsRefreshing(false);
     }
+    setIsRefreshing(false);
+  };
 
-    const refetchFilteredIssues = async () => {
-      if (activeButton === '전체') {
-        const allIssuesResult = await allIssuesRefetch();
-        if (allIssuesResult?.data) {
-          setIssuesList(allIssuesResult.data.pages.flatMap((page) => page.content));
-        }
-      } else {
-        const laneIssuesResult = await laneIssuesRefetch();
-        if (laneIssuesResult?.data) {
-          setIssuesList(laneIssuesResult.data.pages.flatMap((page) => page.content));
-        }
-      }
-    };
-
-    refetchFilteredIssues();
-  }, [activeButton, isRefresh]);
-
-  const onEndReached = () => {
+  const onEndReachedHandler = () => {
     if (activeButton === '전체' && allIssuesHasNextPage) {
       fetchAllIssuesNextPage();
     } else if (activeButton !== '전체' && laneIssuesHasNextPage) {
@@ -78,120 +60,39 @@ const NowScreen = () => {
     }
   };
 
-  useEffect(() => {
-    flatListRef.current?.scrollToIndex({
-      index: 2,
-      viewPosition: 0,
-      viewOffset: -btnTitleHeight,
-    });
-  }, [activeButton]);
-
   return (
-    <SafeAreaView className="justify-center flex-1 bg-white">
-      {isPopularIssuesLoading || isAllIssuesLoading ? (
-        <View className="items-center">
+    <SafeAreaView className="flex-1 bg-white">
+      {isAllIssuesLoading ? (
+        <View className="items-center justify-center flex-1">
           <LoadingCircle width={50} height={50} />
         </View>
       ) : (
-        <>
-          <View className="pt-32 pl-16 pb-11">
-            <FontText text="NOW" className="text-24 leading-34" fontWeight="600" />
-          </View>
-          <FlatList
-            ListHeaderComponent={() =>
-              popularIssues &&
-              popularIssues?.length > 0 && (
-                <View
-                  onLayout={(event) => {
-                    const { height } = event.nativeEvent.layout;
-                    setLayoutHeight(height);
-                  }}
-                >
-                  <View className="pt-24 pb-12 pl-16">
-                    <FontText text="지금 인기" className="text-20 leadiing-20" fontWeight="600" />
-                  </View>
-                  {popularIssues?.map((item, index) => (
-                    <IssueContainer
-                      key={item.id}
-                      issue={item}
-                      isLastItem={index === popularIssues.length - 1}
-                      isHeader
-                    />
-                  ))}
-                </View>
-              )
-            }
-            ref={flatListRef}
-            stickyHeaderIndices={issuesList ? [2] : [0]}
-            getItemLayout={(data, index) => ({ length: 0, offset: layoutHeight, index })}
-            data={['버튼제목', '버튼', ...issuesList, '이슈없음']}
-            renderItem={({ item, index }) => {
-              if (popularIssues) {
-                if (index === 0 && popularIssues?.length < 1) return null;
-                else if (index === 0 && popularIssues?.length > 0)
-                  return (
-                    <View
-                      onLayout={(event) => {
-                        const { height } = event.nativeEvent.layout;
-                        setBtnTitleHeight(height);
-                      }}
-                    >
-                      <LaneButtons
-                        activeButton={activeButton}
-                        setActiveButton={setActiveButton}
-                        titleNotShown={popularIssues?.length < 1}
-                      />
-                    </View>
-                  );
-                else if (index === 1) {
-                  return (
-                    <LaneButtons
-                      activeButton={activeButton}
-                      setActiveButton={setActiveButton}
-                      titleNotShown={true}
-                    />
-                  );
-                } else if (index === 2 && issuesList.length < 1) {
-                  return (
-                    <FontText
-                      text="올라온 이슈가 없어요"
-                      className="text-18 text-gray-999 leading-[700px] text-center"
-                      fontWeight="700"
-                    />
-                  );
-                } else if (index === issuesList.length + 2) return null;
-                else
-                  return (
-                    <IssueContainer
-                      key={item.id}
-                      isLastItem={index === issuesList.length + 1}
-                      issue={item}
-                    />
-                  );
-              }
-              return null;
-            }}
-            ListFooterComponent={() => {
-              if (issuesList.length > 3) {
-                return <View className="h-64 w-999" />;
-              } else if (issuesList.length < 1) {
-                return null;
-              } else {
-                return <Space height={700 - issuesList.length * 100} />;
-              }
-            }}
-            keyExtractor={(item, index) => `${item.id}_${index}`}
-            refreshControl={
-              <RefreshControl
-                onRefresh={() => setRefresh(true)}
-                refreshing={isRefresh}
-                progressViewOffset={-10}
-              />
-            }
-            onEndReached={onEndReached}
-            onEndReachedThreshold={0.4}
-          />
-        </>
+        <FlatList
+          ListHeaderComponent={
+            <>
+              {/* TODO: 인기 이슈 없을 때 기획 나오면 수정 */}
+              {popularIssues && <PopularIssues popularIssues={popularIssues} />}
+              <LaneButtons activeButton={activeButton} setActiveButton={setActiveButton} />
+            </>
+          }
+          data={flattenedData}
+          renderItem={({ item, index }) => (
+            <SingleIssueContainer key={`${item.id}_${index}`} issue={item} />
+          )}
+          ListEmptyComponent={
+            <View className="items-center justify-center flex-1">
+              <FontText text="올라온 이슈가 없어요" className="text-center text-18 text-gray-999" />
+            </View>
+          }
+          ListFooterComponent={() => {
+            if (flattenedData.length > 0) return <View className="h-64" />;
+          }}
+          refreshControl={<RefreshControl onRefresh={onRefreshHandler} refreshing={isRefreshing} />}
+          contentContainerStyle={{ flexGrow: 1 }}
+          onEndReached={onEndReachedHandler}
+          onEndReachedThreshold={0.7}
+          keyExtractor={(item, index) => `${item.id}_${index}`}
+        />
       )}
     </SafeAreaView>
   );
